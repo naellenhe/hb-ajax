7001c2f49eb39c5e6080308787dbdb71ae192b2f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-26 13:02:05 -0800

    completed part2,3

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -26,6 +26,9 @@ function showWeather(evt) {
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
 
+    $.get(url, formData, function (result) {
+        $('#weather-info').html(`Forecast: ${result['forecast']} Temp: ${result['temp']}`);
+    });
 
     // TODO: request weather with that URL and show the forecast in #weather-info
 }
@@ -42,6 +45,20 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    let formInput = {
+        "qty": $("#qty-field").val(),
+        "melon_type": $("#melon-type-field").val(),
+    };
+
+    $.post('/order-melons.json', formInput, function (result) {
+        $('#order-status').html(`${result['code']}: ${result['msg']}`);
+
+        if (result['code'] === 'ERROR') {
+            $('#order-status').addClass('order-error');
+        } else {
+            $('#order-status').removeClass('order-error');
+        }
+    });
 }
 
 $("#order-form").on('submit', orderMelons);
